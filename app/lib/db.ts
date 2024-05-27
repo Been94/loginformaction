1f1d0bf4bdbf27d313b60d8c2a00c7f8e2faee20
been94 <rlagywhd019@naver.com>
2024-05-27 15:47:06 +0900

    # Create prisma model : User, Tweet, Like

---
+++ app/lib/db.ts
@@ -0,0 +1,8 @@
+import { PrismaClient } from "@prisma/client";
+
+const db = new PrismaClient();
+
+async function test() {}
+test();
+
+export default db;
---
+++ prisma/migrations/20240527064530_login_info/migration.sql
@@ -0,0 +1,29 @@
+/*
+  Warnings:
+
+  - Added the required column `bio` to the `User` table without a default value. This is not possible if the table is not empty.
+  - Made the column `email` on table `User` required. This step will fail if there are existing NULL values in that column.
+  - Made the column `password` on table `User` required. This step will fail if there are existing NULL values in that column.
+
+*/
+-- DropIndex
+DROP INDEX "Tweet_tweet_key";
+
+-- RedefineTables
+PRAGMA foreign_keys=OFF;
+CREATE TABLE "new_User" (
+    "id" INTEGER NOT NULL PRIMARY KEY AUTOINCREMENT,
+    "username" TEXT NOT NULL,
+    "email" TEXT NOT NULL,
+    "password" TEXT NOT NULL,
+    "bio" TEXT NOT NULL,
+    "created_at" DATETIME NOT NULL DEFAULT CURRENT_TIMESTAMP,
+    "updated_at" DATETIME NOT NULL
+);
+INSERT INTO "new_User" ("created_at", "email", "id", "password", "updated_at", "username") SELECT "created_at", "email", "id", "password", "updated_at", "username" FROM "User";
+DROP TABLE "User";
+ALTER TABLE "new_User" RENAME TO "User";
+CREATE UNIQUE INDEX "User_username_key" ON "User"("username");
+CREATE UNIQUE INDEX "User_email_key" ON "User"("email");
+PRAGMA foreign_key_check("User");
+PRAGMA foreign_keys=ON;
